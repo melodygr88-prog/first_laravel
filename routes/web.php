9ef934bac28b6855cf63a060c2279c4c783dc4de
melodygr88-prog <melody.gr88@gmail.com>
2025-11-04 15:58:22 +0800

    init push

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/page1', function () {
+    return view('page1');
+});
+Route::get('/page2', function () {
+    return view('page2');
+});
